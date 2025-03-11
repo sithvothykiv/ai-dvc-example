590abf27550c33b298b7c1dbbd986ec785b24042
Sithvothy Kiv <sithvothy.kiv@DMC-068.local>
2025-03-11 16:04:45 +0700

    first commit

---
+++ preprocess.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv("./data/users.csv")
+df["age_group"] = df["Age"].apply(lambda x: "Young" if x < 30 else "Adult")
+df.to_csv("./data/processed_users.csv", index=False)
